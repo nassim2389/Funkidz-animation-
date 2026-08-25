b5dbdd889d35da3db00800db599dc81fcca9bf2d
nassim2389 <Nassim2389@hotmail.com>
2026-08-26 00:05:15 +0300

    fix: auto-hash passwords when creating users from Django admin

--- users/admin.py
+++ users/admin.py
@@ -23,7 +23,9 @@ class UserAdmin(admin.ModelAdmin):
     )
 
     def save_model(self, request, obj, form, change):
-        # Auto-create animateur profile if role is ANIMATEUR
+        # Auto-hash password if entered in plain text
+        if obj.password and not obj.password.startswith(('pbkdf2_sha256$', 'pbkdf2$', 'argon2$', 'bcrypt$')):
+            obj.set_password(obj.password)
         super().save_model(request, obj, form, change)
         if obj.role == User.Role.ANIMATEUR:
             AnimateurProfile.objects.get_or_create(user=obj)
